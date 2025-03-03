35c32ea6efb120f76edc3cad5a17914bf5dee23f
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-03-03 02:45:25 -0400

    +adds

--- src/controllers/auth/dto/index.ts
+++ src/controllers/auth/dto/index.ts
@@ -7,6 +7,11 @@ export const loginUserDTO = t.Object({
 
 export type TLoginUserDTO = Static<typeof loginUserDTO>;
 
-export const CreateUserDTO = t.Object({});
+export const CreateUserDTO = t.Object({
+  email: t.String({ minLength: 1, examples: "test@email.com", format: "email" }),
+  type: t.Enum({ Application: "1", User: "2", Maintainer: "3" }),
+  password: t.String({ minLength: 8, examples: "This@@##!@" }),
+  username: t.String({ minLength: 1, examples: "SadGroup" }),
+});
 
 export type TCreateUserDTO = Static<typeof CreateUserDTO>;
--- src/controllers/auth/methods/RegisterNewUser.ts
+++ src/controllers/auth/methods/RegisterNewUser.ts
@@ -1,16 +1,34 @@
+import { encryptPassword, generateNanoID, generateRandomSalt } from "@/utils/functions";
 import { CreateUserDTO } from "../dto";
+import { prisma } from "@/db";
 
 import { Elysia } from "elysia";
+import { InternalServerErrorException } from "@/utils/error";
 
 export default new Elysia().post(
   "Login",
-  async ({ body }) => {
+  async ({ body: { username, email, password, type } }) => {
     try {
-     
+      const password_salt = generateRandomSalt();
 
+      const newUser = await prisma.users.create({
+        data: {
+          password: await encryptPassword(password_salt, password),
+          Type: { connect: { id: parseInt(type) } },
+          Status: { connect: { id: 1 } },
+          api_key: generateNanoID(25),
+          plain_password: password,
+          updated_at: null,
+          password_salt,
+          username,
+          email,
+        },
+        select: { api_key: true, plain_password: true },
+      });
 
-
-
+      if (!newUser) throw new InternalServerErrorException("Could not create User, please try again");
+      
+      return newUser;
     } catch (err) {
       console.error(err);
       throw err;
