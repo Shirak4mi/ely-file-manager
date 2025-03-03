c8ea0952d13c0c254d3a79c0fe8e6e2f4b66f156
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-03-03 04:37:59 -0400

    Update getOneById.ts

--- src/controllers/files/methods/getOneById.ts
+++ src/controllers/files/methods/getOneById.ts
@@ -1,5 +1,5 @@
-import { convertIPv6ToIPv4, getFileExtension, isDirectory, logger, returnActualOSPath } from "@/utils/functions";
-import { IdBasedTokenDTO, InlineQueryDTO } from "@/common/dto's";
+import { isDirectory, returnActualOSPath } from "@/utils/functions.ts";
+import { IdBasedTokenDTO, InlineQueryDTO } from "@/common/dto's.ts";
 import { NotFoundException } from "@/utils/error";
 import { file as bFile } from "bun";
 import { prisma } from "@/db";
@@ -8,7 +8,7 @@ import { Elysia } from "elysia";
 
 export default new Elysia().decorate("api_key", "" as string).get(
   "get-one-by-id/:id",
-  async ({ params: { id }, query: { inline }, set, api_key, request: req, server }) => {
+  async ({ params: { id }, query: { inline }, set }) => {
     try {
       const doesFileExists = await prisma.metadata.findFirst({
         select: { file_path: true, file_name: true },
@@ -20,43 +20,16 @@ export default new Elysia().decorate("api_key", "" as string).get(
       const { file_path, file_name } = doesFileExists;
 
       const basePath = returnActualOSPath(file_path);
+
       const totalFilePath = (await isDirectory(basePath)) ? basePath + file_name : file_path;
+
       const actualFile = bFile(totalFilePath);
       const contentType = actualFile.type || "application/octet-stream";
-      const cntnLength: string = (actualFile.length as number).toString();
+      const cntnLength = actualFile.size;
 
-      set.headers["Content-disposition"] = inline ? `inline` : `attachment; filename="${actualFile.type}"`;
+      set.headers["Content-disposition"] = inline ? "inline" : `attachment; filename="${actualFile.name}"`;
       set.headers["Content-length"] = cntnLength;
       set.headers["Content-Type"] = contentType;
-
-      const requestedBy = await prisma.users.findFirst({
-        select: { id: true, username: true, email: true },
-        where: { api_key },
-      });
-
-      const requestedToken =
-        req.headers.get("authorization") !== null ? (req.headers.get("authorization") ?? " ").split(" ")[1] : "N/A";
-
-      logger("INFO", "GET ONE FILE BY ID", {
-        requestedBy,
-        requestIpFrom: convertIPv6ToIPv4(server ? server.requestIP.toString() : "N/A"),
-        requestUserAgent: req.headers.get("user-agent"),
-        requestedResource: req.method,
-        requestPath: req.url,
-        Method: req.method,
-        requestedData: {
-          file: {
-            extension: getFileExtension(actualFile.name ?? ""),
-            size: (await actualFile.stat()).size,
-            type: actualFile.type,
-            name: actualFile.name,
-          },
-        },
-        requestedToken,
-        requestStatus: 200,
-        requestAt: new Date(),
-        response: { message: ["File retrieved successfully"], statusCode: 200 },
-      });
 
       return actualFile;
     } catch (err) {
